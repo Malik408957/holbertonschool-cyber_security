dda2981714c0e0ea4104b76a2d5edc0df3974cf4
kali <malikaqakisiyev353@gmail.com>
2025-06-23 14:19:23 -0400

    Add file integrity validator script

--- 2-sha256_validator.sh
+++ 2-sha256_validator.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ "(sha256sum "1" | awk '{print 1')" = "2" ]; then
-  echo "1: OK"
-else
-  echo "1: FAIL"
-fi
+[ "(sha256sum "1" | awk '{print 1')" = "2" ] && echo "1: OK" || echo "1: FAIL"
