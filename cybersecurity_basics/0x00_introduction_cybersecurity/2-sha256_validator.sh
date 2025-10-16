e9d7e75b73c04f07f4a8947b628f99151209e1a0
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-15 21:10:34 -0400

    Fix: Reduce file to 2 lines as required

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-computed_hash=$(sha256sum "$1" | cut -d ' ' -f 1)
-[ "$computed_hash" = "$2" ] && echo "$1: OK" || echo "$1: FAIL"
+computed_hash=$(sha256sum "$1" | cut -d ' ' -f 1) && [ "$computed_hash" = "$2" ] && echo "$1: OK" || echo "$1: FAIL"
