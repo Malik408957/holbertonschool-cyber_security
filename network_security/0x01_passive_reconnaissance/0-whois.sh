758739dae96b95b1707eec5cfccbba7e4cecb304
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-19 22:07:37 -0400

    Fix whois script to output correct CSV format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois -h whois.gandi.net $1 | awk -F':' 'BEGIN{OFS=","} /^Registrant|^Admin|^Tech/{s=$1; sub(/ .*/,"",s)} /^[[:space:]]+[A-Z][a-zA-Z/ -]+:/ && s{gsub(/^[[:space:]]+|[[:space:]]+$/,"",$0); split($0,a,":"); f=s" "a[1]; if(a[1]~/Street/) a[2]=a[2]" "; if(a[1]!~/Name Server|URL|DNSSEC|>>>|https/) print f,a[2]}' > "${1}.csv"
+whois -h whois.gandi.net "$1" | awk -F':' 'BEGIN{OFS=","} /Registrant Name:/{s="Registrant"} /Admin Name:/{s="Admin"} /Tech Name:/{s="Tech"} /^ *[A-Z]/ && s && NF>1{gsub(/^ +| +$/,"",$1); gsub(/^ +| +$/,"",$2); if($2 && $1!~/Name Server|URL/){f=s" "$1; if($1~/Street/) $2=$2" "; print f,$2}}' > "${1}.csv"
