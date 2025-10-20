3f8422f8ec44ab20c683199f5d92748103b5105c
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-19 22:25:53 -0400

    Fix whois script to output correct CSV format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois -h whois.gandi.net "$1" | awk -F':' '/Registrant Name:/{s="Registrant"} /Admin Name:/{s="Admin"} /Tech Name:/{s="Tech"} s && /^ *[A-Z][a-zA-Z\/ -]+:/{gsub(/^ +| +$/,"",$1);gsub(/^ +| +$/,"",$2);if($1!~/Name Server|URL|DNSSEC/&&$2)print s" "$1","($1~/Street/?$2" ":$2)}' > "${1}.csv"
+whois -h whois.gandi.net "$1" | awk -F':' '/Registrant Name:/{s="Registrant";next} /Admin Name:/{s="Admin";next} /Tech Name:/{s="Tech";next} s&&/^ *[A-Z][a-zA-Z\/ -]+:/{gsub(/^ +| +$/,"",$1);gsub(/^ +| +$/,"",$2);if($1!~/Name Server|URL|DNSSEC|Registrar/){f=s" "$1;if($1~/Ext|Fax/&&$1!~/:$/)f=f":";v=($1~/Street/)?$2" ":($2?$2:"");if(f&&v!="")print f","v}}' > "${1}.csv"
