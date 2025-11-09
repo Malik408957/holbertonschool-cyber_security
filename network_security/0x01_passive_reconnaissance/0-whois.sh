b9fefc1903f5c90ef10d1cf220f9590ad0777a51
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-11-09 14:39:04 -0500

    Update 0-whois.sh with current version

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech) / { gsub(/^[ \t]+/, "", $2); field=$1; gsub(/[ \t]+$$ /, "", field); ext = (field ~ /(Phone|Fax) Ext $$/); printf "%s%s,%s\n", field, (ext ? ":" : ""), (length($2)>0 ? $2 : " ") }' > "$1".csv
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech) / { gsub(/^[ \t]+/, "", $2); field=$1; gsub(/[ \t]+$/, "", field); ext=(field ~ /(Phone|Fax) Ext$/); street=(field ~ /Street$/); val=(length($2)>0 ? $2 : " "); if (street) val=val " "; printf "%s%s,%s\n", field, (ext ? ":" : ""), val }' > "$1".csv
