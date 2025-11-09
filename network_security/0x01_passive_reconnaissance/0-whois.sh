5e18fc903dee61c5c4fba95f556e102b84bcd860
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-11-09 13:53:46 -0500

    Update 0-whois.sh to meet requirements (2 lines)

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F':' 'BEGIN{OFS=","} /[Rr]egistrant/&&!/[Rr]egistrar/{s="Registrant"} /[Aa]dmin/&&!/[Rr]egistrar/{s="Admin"} /[Tt]ech/&&!/[Rr]egistrar/{s="Tech"} s&&/^[ ]*[A-Z][a-zA-Z\/ -]+:/&&NF>=2{gsub(/^[ ]+|[ ]+$/,"",$1);gsub(/^[ ]+|[ ]+$/,"",$2);if($1!~/Name Server|URL|DNSSEC|Registrar|>>>/&&$2!="")print s" "$1,($1~/Street/)?$2" ":$2}' > "${1}.csv"
+whois $1 | awk -F':' 'BEGIN{OFS=","} /[Rr]egistrant/&&!/[Rr]egistrar/{s="Registrant"} /[Aa]dmin/&&!/[Rr]egistrar/{s="Admin"} /[Tt]ech/&&!/[Rr]egistrar/{s="Tech"} s&&/^[ ]*[A-Z][a-zA-Z\/ -]+:/&&NF>=2{gsub(/^[ ]+|[ ]+$/,"",$1);gsub(/^[ ]+|[ ]+$/,"",$2);if($1!~/Name Server|URL|DNSSEC|Registrar|>>>/&&$2!="")print s" "$1,($1~/Street/)?$2" ":$2}' > "${1}.csv"
