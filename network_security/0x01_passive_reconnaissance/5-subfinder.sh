4711de21078ec8405e9b593c8396e67a88a25f86
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-11-09 20:58:55 -0500

    Add 5-subfinder.sh script and output file new

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent -nW -oI -o $1.txt | tee >(while read -r host; do ip=$(dig +short "$host" | head -n1); printf "%s,%s\n" "$host" "${ip:-}"; done > tmp && mv tmp "$1.txt")
+subfinder -d $1 -silent -nW  -oI  -o $1.txt
