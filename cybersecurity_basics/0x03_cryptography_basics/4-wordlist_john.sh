f5c8548bfeb13bc3b68bfcaa643de2794ba354d5
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-12-06 22:22:01 -0500

    Task 4: John the Ripper wordlist crack completed

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=raw-md5 "$1" --pot=john.pot 2>/dev/null
-john --show "$1" --pot=john.pot 2>/dev/null | grep : | cut -d: -f2 > 4-password.txt
+john --format=dynamic_0 --wordlist=/usr/share/wordlists/rockyou.txt "$1"; john --show --format=dynamic_0 "$1" | cut -d':' -f2 > 4-password.txt
