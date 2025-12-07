ce556f59613192ed37516044c89fde4a6449b328
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-12-07 15:04:57 -0500

    Completed 5-windows_john task

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 john --wordlist=/usr/share/wordlists/rockyou.txt --format=nt "$1"
-john --show --format=nt "$1" | cut -d: -f2 > 5-password.txt
+john --show --format=nt "$1" \
+    | grep -E ":[^:]+$" \
+    | cut -d: -f2 \
+    | sed 's/^[ \t]*//;s/[ \t]*$//' > 5-password.txt
