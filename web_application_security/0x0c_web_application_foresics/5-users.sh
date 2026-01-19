a9917ee2745e1a3f21716bdb238cb957f9f421ee
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2026-01-19 04:43:11 -0500

    task5 from auth log

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-grep -E "useradd|new user|pam_unix" auth.log \
-| grep -o "user [a-zA-Z0-9_-]*" \
-| awk '{print $2}' \
-| grep -vE "root|nobody" \
-| sort -u \
-| tr '\n' ',' \
-| sed 's/,$//'
+#!/bin/bash 
+grep useradd "auth.log"|cut -d" " -f8|sort|cut -d"=" -f2|tr -d '\n'
